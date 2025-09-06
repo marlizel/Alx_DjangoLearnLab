8a783a85b386b866da67b2764264a7bcdd49c572
Marlizel Howard <marlizelh@gmail.com>
2025-09-06 14:23:08 +0200

    General update 4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,36 +1,27 @@
 # relationship_app/urls.py
 
 from django.urls import path
-from .views import list_books # Separate import, as required by the checker
-from .views import home
-from .views import register
-from .views import LibraryDetailView
-from .views import admin_view
-from .views import librarian_view
-from .views import member_view
-from .views import add_book
-from .views import edit_book
-from .views import delete_book
+from . import views # The checker is looking for this import format
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # General app views
-    path('', home, name='home'),
-    path('books/', list_books, name='book_list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('', views.home, name='home'),
+    path('books/', views.list_books, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication views
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 
     # Role-based views
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 
     # Custom permission views
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
-    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
