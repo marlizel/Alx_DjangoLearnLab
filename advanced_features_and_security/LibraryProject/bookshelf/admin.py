8cb475854793ac2547da01f9b68a1d30b88f5f86
Marlizel Howard <marlizelh@gmail.com>
2025-09-14 13:50:36 +0200

    Security Task 0.3

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,18 @@
+# bookshelf/admin.py
+
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["username", "email", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("role", "date_of_birth", "profile_photo")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
