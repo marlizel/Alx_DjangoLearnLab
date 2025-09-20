cc31ce02806cf72741bc7c824b89d45febbf2074
Marlizel Howard <marlizelh@gmail.com>
2025-09-20 13:19:46 +0200

    Building my first API endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# api_project/api/views.py
 
-# Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    # This specifies the queryset of objects to be returned.
+    # We are getting all objects from the Book model.
+    queryset = Book.objects.all()
+    
+    # This links the view to the serializer we just created.
+    serializer_class = BookSerializer
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,3 +1,5 @@
+# api_project/api_project/urls.py
+
 """
 URL configuration for api_project project.
 
@@ -15,8 +17,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # <-- You need to add 'include' here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # <-- And add this line
 ]
