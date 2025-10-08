c85440f9d0b4a175585b54097fc4f1f92d74ea5f
Marlizel Howard <marlizelh@gmail.com>
2025-10-08 15:36:18 +0200

    Task 0 fix 1

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -21,7 +21,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
